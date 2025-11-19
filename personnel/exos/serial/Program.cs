47586ef30e92fe427e401d49b5754e4305f17494
samumuku <pr13xpj@eduvaud.ch>
2025-11-19 10:16:50 +0100

    add files

---
+++ personnel/exos/serial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("hello");
+
+var maya = new Character();
+
+string json = JsonSerializer.Serialize(maya);
+File.WriteAllText("character.json", json);
+
+string data = System.IO.File.ReadAllText("character.json");
+
+data = JsonSerializer.Deserialize<string>(json);
+
+Console.WriteLine(json);
+
+public class Actor
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Country { get; set; }
+    public bool IsAlive { get; set; }
+}
+
+public class Character
+{
+    public Character()
+    {
+        FirstName = "Maya";
+        LastName = "test";
+        Description = "testtesttest";
+        PlayedBy = null;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Description { get; set; }
+    public Actor PlayedBy { get; set; }
+}
